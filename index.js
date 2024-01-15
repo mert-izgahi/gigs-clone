6d78ea28a62fb659167ab99ad543a7de5aaea2ee
Mert <mert.izgahi@outlook.com>
2024-01-15 16:08:01 +0300

    start config server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import config from "./config.js";
 import connectDB from "./src/helpers/connect-db.js";
+import errorHandler from "./src/middlewares/error-handler-middleware.js";
+import notFound from "./src/middlewares/not-found-middleware.js";
 const app = express();
 
 app.get("/", (req, res) => {
@@ -10,5 +12,7 @@ app.get("/", (req, res) => {
 const PORT = config.PORT;
 app.listen(PORT, async () => {
     await connectDB(config.MONGO_URL);
+    app.use(errorHandler);
+    app.use(notFound);
     console.log(`Server started on http://localhost:${PORT}`);
 });
